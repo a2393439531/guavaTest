1599e5a15d645de0d6735a1ce1c1595570c3922d
liangrui <liangrui_1988@126.com>
2015-06-07 17:04:23 +0800

    缓存抽出公用工具

---
+++ src/org/rui/utils/ICached.java
@@ -0,0 +1,53 @@
+package org.rui.utils;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheStats;
+
+/**
+ * guava cached 共公接口
+ * 
+ * @author liangrui
+ * @date 2015/6/7
+ * @param <K>
+ * @param <V>
+ */
+public interface ICached<K, V>
+{
+	/**
+	 * callable 获取cached 方式
+	 * 
+	 * @param key
+	 * @param callable
+	 * @return
+	 * @throws ExecutionException
+	 */
+	V getCallable(K key, Callable<V> callable) throws ExecutionException;
+
+	/**
+	 * cachedLoader 获取方式
+	 * 
+	 * @param key
+	 * @return
+	 * @throws ExecutionException
+	 */
+	V getLoader(K key) throws ExecutionException;
+
+	/**
+	 * 获取某一项的 缓存
+	 * 
+	 * @param Cached
+	 * @return
+	 */
+	Cache getCache(String Cached);
+
+	/**
+	 * 获取所有guava 缓存
+	 * 
+	 * @return
+	 */
+	Cache[] getCache();
+
+}
